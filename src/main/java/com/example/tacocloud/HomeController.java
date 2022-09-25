bae7787308198e4b9a115abbe007caed052e1286
Thomas Lindley <lindleytc21@gmail.com>
2022-09-25 17:39:28 -0400

    added template file and image

--- src/main/java/com/example/tacocloud/HomeController.java
+++ src/main/java/com/example/tacocloud/HomeController.java
@@ -1,5 +1,11 @@
 package com.example.tacocloud;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class HomeController {
-//    hey I did this in new branch
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
